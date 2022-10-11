b619728d5da94fe05042b39803e4cd5b31d0c311
Simon Weiler <simon.emanuel.weiler@gmail.com>
2022-10-11 12:10:00 +0100

    Add files via upload

--- read_wavesurfer_file_example.m
+++ read_wavesurfer_file_example.m
@@ -18,6 +18,7 @@ list=dir([char(exp_folder) '\*.h5']);
 cd(exp_folder);
 %create figure 
 fig1=figure;set(fig1, 'Position', [200, 200, 2000, 800]);set(gcf,'color','w');
+stimtype=[1 6 3 9 5];
 for i=1:length(list)
 temp=1;
 data = loadDataFile_wavesurfer(list(i).name);
@@ -28,8 +29,17 @@ plot(traces,'k');box off;ylabel('Membrane Voltage (mV)');
 name_stim=list(i).name;
 title(name_stim(1:end-8));
 
+
 %read out sampling rate;
-sampling_rate(i)=data.header.AcquisitionSampleRate;
+sr=data.header.AcquisitionSampleRate;
+
 %total time in seconds of each recording 
-total_length(i)=length(traces)/sampling_rate(i);
+total_length(i)=length(traces)/sr;
 end
+
+%aux info about stimuli
+iv_aux=data.header.StimulusLibrary.Stimuli.element1.Delegate;
+passive_aux=data.header.StimulusLibrary.Stimuli.element6.Delegate;
+ramp_aux=data.header.StimulusLibrary.Stimuli.element3.Delegate;
+rheobase_aux=data.header.StimulusLibrary.Stimuli.element9.Delegate;
+sag_aux=data.header.StimulusLibrary.Stimuli.element5.Delegate;
