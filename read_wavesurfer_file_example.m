6db095f02f7e62d79fb96857131f427919cce445
Simon Weiler <simon.emanuel.weiler@gmail.com>
2022-10-11 11:37:25 +0100

    Add files via upload
    example script to extract data and display it

---
+++ read_wavesurfer_file_example.m
@@ -0,0 +1,35 @@
+
+%% Ephys course 11.10.2021: Readout traces for Rheobase, IV, Passive, Sag and Ramp (SW 2022)
+
+%you need loadDataFile_wavesurfer.m as dependency and test data in folder
+%with  h5 files 
+
+%%  Close all windows and clear all variables
+clear all;close all;
+
+%% put directory of folder with data here!!!
+
+exp_folder='C:\Users\simonw\S1-V1 interaction Dropbox\Simon Weiler\Callosal_L6\SWC_stuff\Code_students_2021\JL00001';
+
+
+%% Read out traces and plot traces 
+%search for file format 
+list=dir([char(exp_folder) '\*.h5']);
+cd(exp_folder);
+%create figure 
+fig1=figure;set(fig1, 'Position', [200, 200, 2000, 800]);set(gcf,'color','w');
+for i=1:length(list)
+temp=1;
+data = loadDataFile_wavesurfer(list(i).name);
+traces=[];traces=data.(['sweep_000',num2str(temp)]).analogScans(:,1);
+%plot
+subplot(round(length(list))/2,round(length(list)/2),i);
+plot(traces,'k');box off;ylabel('Membrane Voltage (mV)');
+name_stim=list(i).name;
+title(name_stim(1:end-8));
+
+%read out sampling rate;
+sampling_rate(i)=data.header.AcquisitionSampleRate;
+%total time in seconds of each recording 
+total_length(i)=length(traces)/sampling_rate(i);
+end
